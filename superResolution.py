1bac56f7da0ce92c2962fec9bc1b515f1e8e55ad
王秋玄 <lolly@wangqiuxuande-MacBook-Air.local>
2018-04-08 01:07:07 +0800

    Resize output image

--- superResolution.py
+++ superResolution.py
@@ -92,7 +92,7 @@ def closure():
     out_HR = net(net_input)
     out_LR = crop_npimage(downsampler(out_HR), d = 32)
 
-    total_loss = mse(out_LR, img_np) 
+    total_loss = mse(out_LR, img_LR_var) 
     
     if tv_weight > 0:
         total_loss += tv_weight * tv_loss(out_HR)
