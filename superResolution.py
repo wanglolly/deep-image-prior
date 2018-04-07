9ab10ac60f419a7eba58dd0c3c4b08b1d39f7d80
王秋玄 <lolly@wangqiuxuande-MacBook-Air.local>
2018-04-07 23:35:00 +0800

    Take out the crop_image

--- superResolution.py
+++ superResolution.py
@@ -29,7 +29,7 @@ path_to_image = 'data/images/LowResolution.png'
 
 #Load Image
 #img_pil = crop_image(get_image(path_to_image, imsize)[0], d=32)
-img_pil = get_image(path_to_image, imsize)[0], d=32
+img_pil = get_image(path_to_image, imsize)
 img_np = pil_to_np(img_pil)     
 if SAVE:
     saveImage("Results/SuperResolution/SR_Original.png", img_np, 4, 12)
