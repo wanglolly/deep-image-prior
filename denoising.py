7a045fec23479aa294fbef46ea28aa2db1e269a0
王秋玄 <lolly@wangqiuxuande-MacBook-Air.local>
2018-04-07 21:31:07 +0800

    Add PSNR comment

--- denoising.py
+++ denoising.py
@@ -35,6 +35,9 @@ GTfilename = "data/images/noise_GT.png"
 GTimg_pil = crop_image(get_image(GTfilename, imsize)[0], d=32)
 GTimg_np = pil_to_np(GTimg_pil) 
 
+#Initial PSNR
+print ('Input PSNR %.3f' % (compare_psnr(GTimg_np, img_np)), '\n', end='') 
+
 #Setup
 INPUT = 'noise' # 'meshgrid'
 pad = 'reflection'
@@ -100,4 +103,4 @@ optimize(OPTIMIZER, p, closure, LR, num_iter)
 out_np = var_to_np(net(net_input))
 if SAVE:
     saveImage("Results/Denoising/Denoising_FinalOutput.png", out_np, factor=13)
-    print ('Final PSNR %.3f' % (compare_psnr(GTimg_np, out_np)), '\r', end='') 
+    print ('Final PSNR %.3f' % (compare_psnr(GTimg_np, out_np)), '\n', end='') 
