d63b1c58ceead3435b904bae5def69856860a84d
王秋玄 <lolly@wangqiuxuande-MacBook-Air.local>
2018-04-07 19:19:40 +0800

    Modify saving parameters

--- utils/common_utils.py
+++ utils/common_utils.py
@@ -101,7 +101,7 @@ def saveImage(path, images_np, nrow =8, factor=1, interpolation=None):
         interpolation: interpolation used in plt.imshow
     """
     n_channels = max(x.shape[0] for x in images_np)
-    assert (n_channels == 3) or (n_channels == 1), "images should have 1 or 3 channels"
+    #assert (n_channels == 3) or (n_channels == 1), "images should have 1 or 3 channels"
     
     images_np = [x if (x.shape[0] == n_channels) else np.concatenate([x, x, x], axis=0) for x in images_np]
 
