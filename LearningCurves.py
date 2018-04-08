f72117ced73d5edfe05053d960ce38e4256ab3fe
王秋玄 <lolly@wangqiuxuande-MacBook-Air.local>
2018-04-08 14:13:44 +0800

    Change to target4: Noise

--- LearningCurves.py
+++ LearningCurves.py
@@ -51,7 +51,7 @@ TargetImage = {'image' : img_np,
                 'imageNoise' : img_noisy_np,
                 'imageShuffle' : img_shuffle_np,
                 'Noise' : random_np}
-Target = 'imageShuffle'
+Target = 'Noise'
 
 #Prepare Loss File
 LossFilename = 'Results/LearningCurve_' + Target + '.csv'
